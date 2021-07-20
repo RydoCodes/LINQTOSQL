155037c0b4cb68d7242fdc9cd29c0d033198758a
Vaibhav <vaibhav.maurya@centricconsulting.com>
2021-07-20 21:19:55 +0530

    Add project files.

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQTOSQL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RydoEntities rydodbContext = new RydoEntities();
+
+         //START-----------------List of Employees.--------------
+            //List<Employees> rydolistemployees = rydodbContext.Employees.ToList();
+            //GridView1.DataSource = rydolistemployees.ToList();
+            //GridView1.DataBind();
+         //END-----------------List of Employees.--------------
+
+
+         // START----------------List of "MALE" Employees.--------
+            IQueryable<Employees> rydolistemployees = from rydoemployee in rydodbContext.Employees
+                                                where rydoemployee.Gender=="Male"
+                                                orderby rydoemployee.Salary descending
+                                                select rydoemployee;
+
+            GridView1.DataSource = rydolistemployees.ToList();
+            GridView1.DataBind();
+
+            //            SELECT
+            //    [Extent1].[ID] AS[ID], 
+            //    [Extent1].[FirstName] AS[FirstName], 
+            //    [Extent1].[LastName] AS[LastName], 
+            //    [Extent1].[Gender] AS[Gender], 
+            //    [Extent1].[Salary] AS[Salary], 
+            //    [Extent1].[DepartmentId]
+            //        AS[DepartmentId]
+            //FROM[dbo].[Employees]
+            //        AS[Extent1]
+            //WHERE N'Male' = [Extent1].[Gender]
+            //        ORDER BY[Extent1].[Salary]
+            //        DESC
+
+         // END----------------List of "MALE" Employees.--------
+        }
+    }
+}
