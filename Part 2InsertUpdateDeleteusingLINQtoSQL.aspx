5c28f876af720a85b851139544cbda8f7e9c3d66
Vaibhav <vaibhav.maurya@centricconsulting.com>
2021-07-21 00:14:43 +0530

    Part 2 Insert Update Delete using LINQ to SQL

---
+++ Part 2InsertUpdateDeleteusingLINQtoSQL.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Part 2InsertUpdateDeleteusingLINQtoSQL.aspx.cs" Inherits="LINQTOSQL.Part_2InsertUpdateDeleteusingLINQtoSQL" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:Button ID="btnGetData" runat="server" OnClick="btnGetData_Click" Text="Get Data" />
+        <asp:Button ID="btnInsertData" runat="server" Text="Insert Tim's Row" OnClick="btnInsertData_Click" />
+        <asp:Button ID="btnUpdateData" runat="server" Text="Update Tim's Salary" OnClick="btnUpdateData_Click" />
+        <asp:Button ID="btnDeleteData" runat="server" Text="Delete Tim's Salary" OnClick="btnDeleteData_Click"/>
+    </form>
+</body>
+</html>
---
+++ Part 2InsertUpdateDeleteusingLINQtoSQL.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQTOSQL
+{
+    public partial class Part_2InsertUpdateDeleteusingLINQtoSQL : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetAllRydoEmployees();
+        }
+
+        private void GetAllRydoEmployees() 
+        {
+            RydoEntities rydodbContext = new RydoEntities();
+            IQueryable<Employees> rydolistemployees = from rydoemployee in rydodbContext.Employees
+                                                      where rydoemployee.Gender == "Male"
+                                                      orderby rydoemployee.Salary descending
+                                                      select rydoemployee;
+
+            GridView1.DataSource = rydolistemployees.ToList();
+            GridView1.DataBind();
+        }
+
+        protected void btnGetData_Click(object sender, EventArgs e)
+        {
+            GetAllRydoEmployees();
+        }
+
+        protected void btnInsertData_Click(object sender, EventArgs e)
+        {
+            using (RydoEntities rydodbContext = new RydoEntities())
+            {
+                Employees newEmployee = new Employees
+                {
+                    FirstName="Tim",
+                    LastName="T",
+                    Gender="Male",
+                    Salary=55000,
+                    DepartmentId=1
+                };
+
+                rydodbContext.Employees.Add(newEmployee);
+                rydodbContext.SaveChanges();
+            }
+
+            GetAllRydoEmployees();
+        }
+
+        protected void btnUpdateData_Click(object sender, EventArgs e)
+        {
+            using (RydoEntities rydodbContext = new RydoEntities())
+            {
+                Employees rydoemployee = rydodbContext.Employees.FirstOrDefault(x => x.ID == 9);
+
+                rydoemployee.Salary = 65000;
+                rydodbContext.SaveChanges();
+            }
+        }
+
+        protected void btnDeleteData_Click(object sender, EventArgs e)
+        {
+            using (RydoEntities rydodbContext = new RydoEntities())
+            {
+                Employees rydoemployee = rydodbContext.Employees.FirstOrDefault(x => x.ID == 9);
+                rydodbContext.Employees.Remove(rydoemployee);
+                rydodbContext.SaveChanges();
+            }
+        }
+    }
+}
